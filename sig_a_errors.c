d8a59db3aeb22c653738a0116efd70f25e0b8a92
clde-ber <clde-ber@student.42.fr>
2021-05-26 09:40:35 +0200

    ctrl \ ok

--- sig_a_errors.c
+++ sig_a_errors.c
@@ -6,7 +6,7 @@
 /*   By: clde-ber <clde-ber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 13:56:27 by clde-ber          #+#    #+#             */
-/*   Updated: 2021/05/25 17:51:54 by clde-ber         ###   ########.fr       */
+/*   Updated: 2021/05/26 09:39:36 by clde-ber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,10 @@ void	handle_signal(int code)
 	else if (code == 3)
 	//ctrl-antislash
 	{
-		g_sig.sig = 2;
 		if (g_sig.boolean == 1)
+		{
+			g_sig.sig = 2;
 			write(1, "Quit (core dumped)\n", 19);
-		else
-			write(1, "Quit (core dumped)\n***minishell*** > ", 37);
+		}
 	}
 }
